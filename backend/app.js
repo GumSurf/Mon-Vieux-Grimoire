5f4a113f727330427832470fea0849821a2ccadc
GumSurf <gabriel.christe@gmail.com>
2023-07-22 21:26:34 +0200

    Add: app

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://GumSurf:RW6UmgSjwKozlA3l@monvieuxgrimoire.oop7i2i.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content', 'Accept', 'Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
+/*app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});*/
+
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+    res.send('Bienvenue sur mon serveur !');
+    next();
+});
+
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
+
