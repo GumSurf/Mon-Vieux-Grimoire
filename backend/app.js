e47dd0f616e1081521ee95a093fb81c1822e7dc6
GumSurf <gabriel.christe@gmail.com>
2023-07-25 00:31:46 +0200

    Delete: useless commentary

--- backend/app.js
+++ backend/app.js
@@ -23,13 +23,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-/*app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});*/
-
 app.use(express.json());
 
 app.get('/', (req, res, next) => {
