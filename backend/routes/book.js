61bfe86a67e56e924e1c84cf85a91b8c8adc14be
GumSurf <gabriel.christe@gmail.com>
2023-07-25 00:32:07 +0200

    Add: uploadImage in the route

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 const router = express.Router();
-const multer = require('../middleware/multer-config');
+const upload = require('../middleware/multer-config');
 
 const bookCtrl = require('../controllers/book');
+const uploadImageMiddleware = require('../middleware/uploadImage');
 
 router.get('/', bookCtrl.getAllBook);
 router.get('/bestrating', bookCtrl.bestRating);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/:id', bookCtrl.getOneBook);
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
-router.post('/:id/rating', auth, bookCtrl.rating);
 
+// Utilisez le middleware de Multer ici
+router.post('/', auth, upload.single('image'), uploadImageMiddleware, bookCtrl.createBook);
+
+router.put('/:id', auth, upload.single('image'), uploadImageMiddleware, bookCtrl.modifyBook);
+router.post('/:id/rating', auth, bookCtrl.rating);
 
 module.exports = router;
