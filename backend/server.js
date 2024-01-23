3b77584dbc06d4f40367007ef3f7f13fdabe77d7
GumSurf <gabriel.christe@gmail.com>
2024-01-23 18:22:17 +0100

    Add: heroku commit

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,6 @@ const app = require('./app');
 app.set('port', process.env.PORT || 5001)
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 5001);
+const PORT = process.env.PORT || 5001;
+
+server.listen(process.env.PORT || 5001, () => console.log("listening to port " + PORT));
