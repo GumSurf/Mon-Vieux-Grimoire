4ddcb1aad94ac3b4e1be24ebf1e3f2db8fdf0d20
GumSurf <gabriel.christe@gmail.com>
2024-07-10 17:01:11 +0200

    Add: localhost cors

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 
 const corsOptions = {
-    origin: 'https://gumsurf.github.io',
+    origin: ['https://gumsurf.github.io', 'http://localhost:3000/P7-Dev-Web-livres'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content', 'Accept', 'Content-Type', 'Authorization']
 };
