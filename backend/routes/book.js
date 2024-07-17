8ee56def271d34dfad99d88829662693d1495ce2
GumSurf <gabriel.christe@gmail.com>
2024-07-17 23:40:20 +0200

    Edit: routes for don't use multer-config

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -6,15 +6,24 @@ const upload = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 const uploadImageMiddleware = require('../middleware/uploadImage');
 
+const Multer = require('multer');
+
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024  // Limite de taille du fichier, ajustez selon vos besoins
+    }
+});
+
 router.get('/', bookCtrl.getAllBook);
 router.get('/bestrating', bookCtrl.bestRating);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/:id', bookCtrl.getOneBook);
 
 // Utilisez le middleware de Multer ici
-router.post('/', auth, upload.single('image'), uploadImageMiddleware, bookCtrl.createBook);
+router.post('/', auth, multer.single('image'), bookCtrl.createBook);
 
-router.put('/:id', auth, upload.single('image'), uploadImageMiddleware, bookCtrl.modifyBook);
+router.put('/:id', auth, multer.single('image'), bookCtrl.modifyBook);
 router.post('/:id/rating', auth, bookCtrl.rating);
 
 module.exports = router;
